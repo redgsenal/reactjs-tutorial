ce59d615da3d46669fd1128d107135b0709df7bd
LAPTOP-QGFGER35\Debbie Jane Paz <reggie.senal@gmail.com>
2025-07-07 07:27:21 +0800

    using callback refs

--- src/components/RefDemo.jsx
+++ src/components/RefDemo.jsx
@@ -4,11 +4,20 @@ class RefDemo extends Component {
 	constructor(props) {
 		super(props);
 		this.inputRef = React.createRef();
+		this.callBackRef = null;
+		this.setCallBackRef = (element) => {
+			this.callBackRef = element;
+			console.log("Callback ref set: ", this.callBackRef);
+		};
 	}
 
 	componentDidMount() {
 		// Focus the input field when the component mounts
 		this.focusInput();
+		if (this.callBackRef) {
+			console.log("Callback Input field has focus and is ready: ", this.callBackRef);
+			this.callBackRef.focus();
+		}
 		console.log("RefDemo component mounted: ", this.inputRef);
 	}
 
@@ -28,7 +37,13 @@ class RefDemo extends Component {
 	render() {
 		return (
 			<div>
-				<input type="text" ref={this.inputRef} />
+				<input name="createRefInput" type="text" ref={this.inputRef} placeholder="Input with createRef" />
+				<input
+					name="callbackRefInput"
+					type="text"
+					ref={this.setCallBackRef}
+					placeholder="Input with CallbackRef is focus"
+				/>
 				<button onClick={this.focusInput}>Focus Input</button>
 				<button onClick={this.showInputValue}>Show Input Value</button>
 				<button onClick={this.clearInputValue}>Clear Input Value</button>
